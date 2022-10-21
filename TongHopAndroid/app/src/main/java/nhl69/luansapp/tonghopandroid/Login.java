9749a2576d5fc737038c9619ba2b56db9d6342b9
Hải Luân Nguyễn <nguyenhailuan609@gmail.com>
2022-10-21 19:21:34 +0700

    commit chuc nang dang nhap

--- TongHopAndroid/app/src/main/java/nhl69/luansapp/tonghopandroid/Login.java
+++ TongHopAndroid/app/src/main/java/nhl69/luansapp/tonghopandroid/Login.java
@@ -10,11 +10,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
     private Button loginbtn, createbtn;
     private CheckBox showcb;
-    private EditText pass;
+    private EditText username, pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +26,7 @@ public class Login extends AppCompatActivity {
         createbtn = (Button) findViewById(R.id.btncreate);
         showcb = (CheckBox) findViewById(R.id.cbshowp);
         pass = (EditText) findViewById(R.id.pass);
+        username = (EditText) findViewById(R.id.username) ;
 
         createbtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,9 +50,14 @@ public class Login extends AppCompatActivity {
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent();
-                intent.setClass(Login.this, List.class);
-                startActivity(intent);
+                if(username.getText().toString().trim().equals("hailuan609") && pass.getText().toString().trim().equals("abc")){
+                    Intent intent = new Intent();
+                    intent.setClass(Login.this, List.class);
+                    startActivity(intent);
+                }
+                else
+                    Toast.makeText(Login.this,"Tên đăng nhập hoặc mật khẩu chưa đúng!", Toast.LENGTH_SHORT).show();
+
             }
         });
     }
